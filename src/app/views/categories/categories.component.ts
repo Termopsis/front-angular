9c793b82b4f86cfc0ba79e092fc20f4fb6d6ec25
Termopsis <leo_dialog@mail.ru>
2020-08-26 23:21:32 +0300

    Заполнение данных sidebar из бэкэнда!

--- src/app/views/categories/categories.component.ts
+++ src/app/views/categories/categories.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {DataHandlerService} from '../../service/data-handler.service';
+import {Category} from '../../model/Category';
+import {Line, printLine} from 'tslint/lib/verify/lines';
 
 @Component({
   selector: 'app-categories',
@@ -7,9 +10,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoriesComponent implements OnInit {
 
-  constructor() { }
+  categories: Category[];
 
+  constructor(private dataHandler: DataHandlerService) {
+  }
+
+  // метод вызывается авто после иниц компонента
   ngOnInit(): void {
+    this.categories = this.dataHandler.getCategories();
+    // console.log(this.categories);
   }
 
 }
