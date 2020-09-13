b3c9281100bc478137cc742ccb8306c0c4d343ed
Termopsis <leo_dialog@mail.ru>
2020-09-13 04:41:52 +0300

    added some vue in task.

--- src/app/views/tasks/tasks.component.ts
+++ src/app/views/tasks/tasks.component.ts
@@ -1,19 +1,21 @@
-import {Component, OnInit} from '@angular/core';
+import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
 import {DataHandlerService} from "../../service/data-handler.service";
 import {Task} from 'src/app/model/Task';
-import {MatTableDataSource} from "@angular/material";
+import {MatPaginator, MatSort, MatTableDataSource} from '@angular/material';
 
 @Component({
   selector: 'app-tasks',
   templateUrl: './tasks.component.html',
   styleUrls: ['./tasks.component.css']
 })
-export class TasksComponent implements OnInit {
+export class TasksComponent implements OnInit, AfterViewInit {
 
   // поля для таблицы (те, что отображают данные из задачи - должны совпадать с названиями переменных класса)
   private displayedColumns: string[] = ['color', 'id', 'title', 'date', 'priority', 'category'];
   private dataSource: MatTableDataSource<Task>; // контейнер - источник данных для таблицы
 
+  @ViewChild(MatPaginator,{static: false}) private paginator: MatPaginator;
+  @ViewChild(MatSort,{static: false}) private sort: MatSort;
 
   tasks: Task[];
 
@@ -27,6 +29,10 @@ export class TasksComponent implements OnInit {
     this.dataSource = new MatTableDataSource();
 
     this.refreshTable();
+  }
+
+  ngAfterViewInit(): void {
+    this.addTableObjects();
   }
 
 
@@ -45,7 +51,7 @@ export class TasksComponent implements OnInit {
       return task.priority.color;
     }
 
-    return '#bdbdbd';
+    return 'rgba(246,246,246,0.62)';
 
   }
 
@@ -53,7 +59,31 @@ export class TasksComponent implements OnInit {
   private refreshTable() {
 
     this.dataSource.data = this.tasks; // обновить источник данных (т.к. данные массива tasks обновились)
+    this.addTableObjects();
 
+    this.dataSource.sortingDataAccessor = (task, colName) => {
+
+      switch (colName) {
+        case 'priority': {
+          return task.priority ? task.priority.id : null;
+        }
+        case 'category': {
+          return task.category ? task.category.title : null;
+        }
+        case 'date': {
+          return task.date ? task.date : null;
+        }
+        case 'title': {
+          return task.title;
+        }
+      }
+    };
 
   }
+
+  private addTableObjects() {
+    this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
+  }
+
 }
