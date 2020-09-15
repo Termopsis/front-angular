4764780caf354b0beb9b6cd269c3ccf4ea5f0f20
Termopsis <leo_dialog@mail.ru>
2020-09-15 21:51:02 +0300

    Added input in tasks and category

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Task} from 'src/app/model/Task';
+import {DataHandlerService} from 'src/app/service/data-handler.service';
+import {Category} from 'src/app/model/Category';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styles: []
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Todo';
+  tasks: Task[];
+  categories: Category[];
+
+  constructor(
+    private dataHandlerService: DataHandlerService,
+  ) {
+  }
+
+  ngOnInit(): void {
+    this.dataHandlerService.getAllTasks().subscribe(tasks => this.tasks = tasks);
+    this.dataHandlerService.getAllCategories().subscribe(categories => this.categories = categories);
+  }
+
 }
